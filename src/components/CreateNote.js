652ddb0fe5e73b92034753d2c2493236505df7ce
Tyler <62820237+tthikida@users.noreply.github.com>
2023-06-05 13:09:53 -0700

    styling createNote

--- src/components/CreateNote.js
+++ src/components/CreateNote.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
 function CreateNote(props) {
+    const [extended, setExtended] = useState(false);
+    
+    function extendTextArea() {
+        setExtended(true);
+    }
+
+    function addNote() {
+        setExtended(false);
+    }
+
     return(
         <div className={"createNote"}>
-            <input className={"titleInput"} placeholder={"Title"} />
-            <input className={"description"} placeholder={"description"} />
-            <p>jfjsald jl;ask;flkj;kl fjkj ;lak j;lf;jklasjk;asfdkasfdkasdfkafsd;af ds;f das;lafds;af dskafsdkafds;afsd;fdas;dl fkjwa;ljf a;sdljf ;alskj f;laksj f;laksj f;laksdjf ;lkasjf;klasj f;lkasjf;lkasdjf ;lkdsjf ;olkwasdj f;oawsej f;oqwej rfoijw e;lkasdmj vlasjd vkl;hawesd;ioguawer;otgj oaeriujgp[wioag l;keasj glkasdj gfvk.jasdcjn,vmndcfvg,mhjfzs,vmjzsdflkvj zsdlk;jv .zsd,fg;lkwaej g;lkawsjg 'piawejt 'piwe t/.,sdmg,./sdmg;lmmszd ;lkgv SD :LKgj:SLDK ;kl SDl;kgj ;slkdjg l;ksjg;lkj</p>
+            <input className={"titleInput"} placeholder={"Title"} onClick={extendTextArea} />
+            <textarea className={"description"} placeholder={"Write note here"} rows={extended ? 3 : 1}/>
+            <button onClick={addNote}>Add</button>
         </div>
     )
 }
