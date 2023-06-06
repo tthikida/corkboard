aa4e3ea245f2ff97d666b2a090207ba612ffb0f4
Tyler <62820237+tthikida@users.noreply.github.com>
2023-06-06 15:38:07 -0700

    still stylin background pattern add icon delete icon

--- src/notes.js
+++ src/notes.js
@@ -2,7 +2,7 @@ const notes = [
     {
         key: 1,
         title: "the title1",
-        content: "the description1 jskdjdkasj f kjasl; fl; fj jf asdj flksaj flsdfjfajslfjsafj j fjsadfkl jas;dfjs fjlksadlf jasf j;lsjdf; lasfj; sa jfs ldjfjasjfsdaljfj;lsd j;l ajs;lkj jklasj fasdj lfasd;jkafs d;saf;dj;kj ask jafdjk;la sfdfaj s; asjf;ajk s; fk"
+        content: "the description1"
     },
     {
         key: 2,
