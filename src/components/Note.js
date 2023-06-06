000c4d89ad87f2ba3fc5d4283b6a098f17b228ac
Tyler <62820237+tthikida@users.noreply.github.com>
2023-06-06 16:01:48 -0700

    thingsn are starting to look pretty :)

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import DeleteIcon from '@mui/icons-material/Delete';
 function Note(props) {
+    const [isMouseOver, setIsMouseOver] = useState(false);
     function handleClick() {
         props.onDelete(props.id)
     }
+
+    function handleMouseOver() {
+        setIsMouseOver(true);
+    }
     
+    function handleMouseOut() {
+        setIsMouseOver(false);
+    }
+
     return (
         <div className={"note"}>
             <h1>{props.title}</h1>
-            {/*<hr />*/}
             <p>{props.content}</p>
             {/*<button onClick={handleClick}>Delete</button>*/}
-            <DeleteIcon classname={"deleteButton"} onClick={handleClick} style={{color:"grey"}}/>
+            <DeleteIcon 
+                classname={"deleteButton"} 
+                onClick={handleClick} 
+                style={isMouseOver ? {color:"black"} : {color:"grey"}} 
+                onMouseEnter={handleMouseOver}
+                onMouseLeave={handleMouseOut}
+            />
         </div>
     )
 }
